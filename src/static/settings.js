cba269fb89db0f8839d0c951470eb5bfb7941159
Mehmet <mehmet.h.yurdakul@gmail.com>
2022-12-11 19:18:11 +0100

    changed duration of reservation

--- src/static/settings.js
+++ src/static/settings.js
@@ -1,7 +1,7 @@
 const initialSettings = {
   eventFrequency: 10, //seconds
   eventChance: 1, //percent
-  reservationDuration: 30 * 1 * 1000, //milliseconds
+  reservationDuration: 120 * 1 * 1000, //milliseconds
   imageWidth: 720,
   imageHeight: 480,
   version: "0.0.4",
