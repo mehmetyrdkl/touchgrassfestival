6cf2d19c249c0b44a42191d21df736865774fabd
Mehmet <mehmet.h.yurdakul@gmail.com>
2023-01-12 11:15:12 +0100

    reverted all tests

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
